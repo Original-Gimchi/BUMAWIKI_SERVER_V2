7951d53d41d9d1ad3b59643ec4b4304214d08bf7
마현우 <mhw35689402@gmail.com>
2024-04-10 08:57:45 +0900

    refactor(docs): DocsPopularInformationService 리팩토링

--- src/main/java/com/project/bumawiki/domain/docs/presentation/dto/response/DocsPopularResponseDto.java
+++ src/main/java/com/project/bumawiki/domain/docs/presentation/dto/response/DocsPopularResponseDto.java
@@ -1,19 +1,15 @@
 package com.project.bumawiki.domain.docs.presentation.dto.response;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.project.bumawiki.domain.docs.domain.type.DocsType;
 
-public class DocsPopularResponseDto {
-	@JsonProperty
-	private final String title;
-	@JsonProperty
-	private final int enroll;
-	@JsonProperty
-	private final DocsType docsType;
-	@JsonProperty
-	private final long thumbsUpsCounts;
+public record DocsPopularResponseDto(
+	String title,
+	Integer enroll,
+	DocsType docsType,
+	Long thumbsUpsCounts
+) {
 
-	public DocsPopularResponseDto(String title, int enroll, DocsType docsType, long thumbsUpsCounts) {
+	public DocsPopularResponseDto(String title, Integer enroll, DocsType docsType, Long thumbsUpsCounts) {
 		this.title = title;
 		this.enroll = enroll;
 		this.docsType = docsType;
