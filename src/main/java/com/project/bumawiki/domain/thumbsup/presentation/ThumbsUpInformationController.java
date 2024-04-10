7e5a350fbe0fce2550b4a4fab53cb3def2d305d2
마현우 <mhw35689402@gmail.com>
2024-04-10 09:25:52 +0900

    refactor(thumbsup): ThumbsUpInformationService 리팩토링

--- src/main/java/com/project/bumawiki/domain/thumbsup/presentation/ThumbsUpInformationController.java
+++ src/main/java/com/project/bumawiki/domain/thumbsup/presentation/ThumbsUpInformationController.java
@@ -2,24 +2,29 @@ package com.project.bumawiki.domain.thumbsup.presentation;
 
 import java.util.List;
 
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.project.bumawiki.domain.thumbsup.domain.repository.CustomThumbsUpRepository;
 import com.project.bumawiki.domain.thumbsup.presentation.dto.ThumbsUpResponseDto;
-import com.project.bumawiki.domain.thumbsup.service.ThumbsUpInformationService;
+import com.project.bumawiki.global.util.SecurityUtil;
 
 import lombok.RequiredArgsConstructor;
 
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/thumbs/up")
-@RestController
 public class ThumbsUpInformationController {
-	private final ThumbsUpInformationService thumbsUpInformationService;
+	private final CustomThumbsUpRepository customThumbsUpRepository;
 
 	@GetMapping("/get")
-	public ResponseEntity<List<ThumbsUpResponseDto>> getThumbsUp() {
-		return ResponseEntity.ok(thumbsUpInformationService.getThumbsUpList());
+	@ResponseStatus(HttpStatus.OK)
+	public List<ThumbsUpResponseDto> getThumbsUp() {
+		return customThumbsUpRepository.getUserThumbsUp(
+			SecurityUtil.getCurrentUserWithLogin()
+		);
 	}
 }
