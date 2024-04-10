35e78092a7ad6241eb2dc5af46cb55b1a2c72425
마현우 <mhw35689402@gmail.com>
2024-04-10 11:40:16 +0900

    fix(thumbsup): record에 있는 getter 제거

--- src/main/java/com/project/bumawiki/domain/thumbsup/presentation/dto/ThumbsUpRequestDto.java
+++ src/main/java/com/project/bumawiki/domain/thumbsup/presentation/dto/ThumbsUpRequestDto.java
@@ -3,7 +3,6 @@ package com.project.bumawiki.domain.thumbsup.presentation.dto;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
-@Getter
 public record ThumbsUpRequestDto(
 	@NotNull
 	Long docsId
