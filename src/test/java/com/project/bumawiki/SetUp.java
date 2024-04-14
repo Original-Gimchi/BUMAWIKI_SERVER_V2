2721dd3dcf42166a5ef004db2e0324c2e5f478d4
NameIsUser06 <woosoungcho@gmail.com>
2024-04-15 00:13:46 +0900

    feat(test): truncate setup 작성

---
+++ src/test/java/com/project/bumawiki/SetUp.java
@@ -0,0 +1,52 @@
+package com.project.bumawiki;
+
+import com.project.bumawiki.domain.docs.domain.Docs;
+
+import com.project.bumawiki.domain.docs.domain.type.DocsType;
+
+import com.project.bumawiki.domain.user.domain.User;
+
+import com.project.bumawiki.domain.user.domain.authority.Authority;
+
+import jakarta.persistence.EntityManager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+public class SetUp {
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	@Autowired
+	private EntityManager entityManager;
+
+	@Transactional
+	public void cleanUp() {
+		jdbcTemplate.execute("SET REFERENTIAL_INTEGRITY FALSE");
+
+		entityManager.getMetamodel().getEntities().stream().map(entity ->{
+			jdbcTemplate.execute("TRUNCATE TABLE "+ entity.getName());
+			return null;
+		});
+
+		jdbcTemplate.execute("SET REFERENTIAL_INTEGRITY TRUE");
+
+		setUpTestData();
+	}
+
+	private void setUpTestData() {
+		// Docs docs = new Docs("testTitle", 2024, DocsType.STUDENT);
+		// entityManager.persist(docs);
+
+		User user = User.builder()
+			.name("test")
+			.email("test@bssm.hs.kr")
+			.enroll(2024)
+			.authority(Authority.USER)
+			.nickName("test nickname")
+			.build();
+		entityManager.persist(user);
+	}
+}
