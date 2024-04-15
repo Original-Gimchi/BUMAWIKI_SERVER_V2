0a97bd513ac7b92e78ba8cb30dca279132050a4e
qlido <jason7530@naver.com>
2024-04-15 14:59:14 +0900

    refactor(auth): null처리 수정

--- src/main/java/com/project/bumawiki/domain/user/domain/repository/UserRepository.java
+++ src/main/java/com/project/bumawiki/domain/user/domain/repository/UserRepository.java
@@ -1,11 +1,9 @@
 package com.project.bumawiki.domain.user.domain.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.project.bumawiki.domain.user.domain.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-	Optional<User> findByEmail(String email);
+	User findByEmail(String email);
 }
