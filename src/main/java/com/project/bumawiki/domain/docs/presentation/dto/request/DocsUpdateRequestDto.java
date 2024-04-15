b423a921fe65ad118d178cc246c0039126936339
bo <jacobhboy66@gmail.com>
2024-04-15 12:28:03 +0900

    fix(docs): int로 돼서 notNull 작동 안하던거 수정

--- src/main/java/com/project/bumawiki/domain/docs/presentation/dto/request/DocsUpdateRequestDto.java
+++ src/main/java/com/project/bumawiki/domain/docs/presentation/dto/request/DocsUpdateRequestDto.java
@@ -1,8 +1,7 @@
 package com.project.bumawiki.domain.docs.presentation.dto.request;
 
-import lombok.Getter;
-
 import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
 
 @Getter
 public class DocsUpdateRequestDto {
@@ -11,5 +10,5 @@ public class DocsUpdateRequestDto {
     private String contents;
 
     @NotNull(message = "업데이트할 문서의 버전이 전달되지 않았습니다.")
-    private int updatingVersion;
+    private Integer updatingVersion;
 }
