514e61bd0c1a1a77a5a7b5e674cc5b416629178a
마현우 <mhw35689402@gmail.com>
2024-04-16 23:58:15 +0900

    test(docs): 제목으로_문서_상세_조회하기 테스트 코드 작성

---
+++ src/test/java/com/project/bumawiki/domain/docs/service/QueryDocsServiceTest.java
@@ -0,0 +1,71 @@
+package com.project.bumawiki.domain.docs.service;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.project.bumawiki.domain.docs.domain.Docs;
+import com.project.bumawiki.domain.docs.domain.VersionDocs;
+import com.project.bumawiki.domain.docs.domain.repository.DocsRepository;
+import com.project.bumawiki.domain.docs.domain.repository.VersionDocsRepository;
+import com.project.bumawiki.domain.docs.presentation.dto.response.DocsResponseDto;
+import com.project.bumawiki.domain.user.domain.User;
+import com.project.bumawiki.domain.user.domain.authority.Authority;
+import com.project.bumawiki.domain.user.domain.repository.UserRepository;
+import com.project.bumawiki.global.service.ServiceTest;
+
+class QueryDocsServiceTest extends ServiceTest {
+
+	@Autowired
+	private DocsRepository docsRepository;
+
+	@Autowired
+	private QueryDocsService queryDocsService;
+
+	@Autowired
+	private VersionDocsRepository versionDocsRepository;
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Test
+	void 제목으로_문서_상세_조회하기() {
+		// given
+		User user = fixtureGenerator.giveMeBuilder(User.class)
+			.setNull("id")
+			.set("authority", Authority.USER)
+			.setNull("thumbsUps")
+			.sample();
+
+		userRepository.save(user);
+
+		Docs docs = fixtureGenerator.giveMeBuilder(Docs.class)
+			.setNull("id")
+			.setNull("versionDocs")
+			.sample();
+		String title = docs.getTitle();
+
+		docsRepository.save(docs);
+
+		VersionDocs versionDocs = fixtureGenerator.giveMeBuilder(VersionDocs.class)
+			.set("version", 1)
+			.set("docs", docs)
+			.set("user", user)
+			.sample();
+
+		versionDocsRepository.save(versionDocs);
+
+		// when
+		DocsResponseDto dto = queryDocsService.findDocs(title);
+
+		// then
+		assertAll(
+			() -> {
+				assertThat(dto).isNotNull();
+				assertThat(dto.title()).isEqualTo(title);
+			}
+		);
+	}
+}
