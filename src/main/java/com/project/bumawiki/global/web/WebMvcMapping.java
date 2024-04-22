846257110d4dd2fbdc41a8f9d7260303182059e0
NameIsUser06 <woosoungcho@gmail.com>
2024-04-23 08:45:00 +0900

    refactor(auth): WebMvcMapping 구현

--- src/main/java/com/project/bumawiki/global/web/WebMvcMapping.java
+++ src/main/java/com/project/bumawiki/global/web/WebMvcMapping.java
@@ -2,10 +2,25 @@ package com.project.bumawiki.global.web;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.project.bumawiki.domain.auth.implementation.AuthReader;
+import com.project.bumawiki.domain.auth.implementation.AuthUpdater;
+import com.project.bumawiki.domain.auth.interceptor.AuthInterceptor;
+import com.project.bumawiki.domain.auth.util.JwtParser;
+import com.project.bumawiki.domain.user.implementation.UserReader;
+
+import lombok.RequiredArgsConstructor;
+
 @Configuration
+@RequiredArgsConstructor
 public class WebMvcMapping implements WebMvcConfigurer {
+	private final JwtParser jwtParser;
+	private final AuthUpdater authUpdater;
+	private final AuthReader authReader;
+	private final UserReader userReader;
+
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
@@ -17,5 +32,10 @@ public class WebMvcMapping implements WebMvcConfigurer {
 			)
 			.allowedMethods("*")
 			.allowedHeaders("*");
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new AuthInterceptor(jwtParser, authUpdater, authReader, userReader));
 	}
 }
