ff2bd5762e9a5dea61303979903d5e4298a6b08c
qlido <jason7530@naver.com>
2024-04-23 10:56:06 +0900

    feat(image): S3 -> R2

--- src/main/java/com/project/bumawiki/global/config/s3/S3Config.java
+++ src/main/java/com/project/bumawiki/global/config/s3/S3Config.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
@@ -19,13 +18,25 @@ class S3Config {
 	@Value("${aws.s3.secret-key}")
 	private String secretKey;
 
+	@Value("${aws.s3.region}")
+	private String region;
+
+	@Value("${aws.s3.end-point-url}")
+	private String endPointUrl;
+
 	@Bean
 	AmazonS3 amazonS3Client() {
 		return AmazonS3ClientBuilder.standard()
 			.withCredentials(
 				new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey))
 			)
-			.withRegion(Regions.AP_NORTHEAST_2)
+			// .withRegion(Regions.AP_NORTHEAST_2)
+			.withEndpointConfiguration(
+				new AmazonS3ClientBuilder.EndpointConfiguration(
+					endPointUrl,
+					region
+				)
+			)
 			.build();
 	}
 }
