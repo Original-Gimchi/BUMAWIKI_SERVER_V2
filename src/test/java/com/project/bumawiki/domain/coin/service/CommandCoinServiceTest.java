b675dd9334d2c67c52726b2b56954da3e8c0388d
마현우 <mhw35689402@gmail.com>
2024-04-28 20:32:19 +0900

    refactor(coin): 안 쓰는 변수 삭제

--- src/test/java/com/project/bumawiki/domain/coin/service/CommandCoinServiceTest.java
+++ src/test/java/com/project/bumawiki/domain/coin/service/CommandCoinServiceTest.java
@@ -60,8 +60,6 @@ class CommandCoinServiceTest extends ServiceTest {
 		@RepeatedTest(REPEAT_COUNT)
 		void 잔고가_충분하지_않을_경우() {
 			// given
-			Price price = priceRepository.getRecentPrice();
-
 			User user = FixtureGenerator.getDefaultUserBuilder().sample();
 
 			userRepository.save(user);
@@ -201,8 +199,149 @@ class CommandCoinServiceTest extends ServiceTest {
 		}
 	}
 
-	@RepeatedTest(REPEAT_COUNT)
-	void 코인_판매하기() {
+	@Nested
+	class 코인_판매하기 {
+		@RepeatedTest(REPEAT_COUNT)
+		void 잔고가_충분하지_않을_경우() {
+			// given
+			Price price = priceRepository.getRecentPrice();
 
+			User user = FixtureGenerator.getDefaultUserBuilder().sample();
+
+			userRepository.save(user);
+
+			Long coinCount = 0L;
+			Long coinPrice = 0L;
+
+			while (coinCount * coinPrice <= 0) {
+				coinCount = FixtureGenerator.getDefaultLongArbitrary()
+					.greaterOrEqual(0L)
+					.sample();
+				coinPrice = FixtureGenerator.getDefaultLongArbitrary()
+					.greaterOrEqual(1000000L)
+					.sample();
+			}
+
+			TradeWithoutTradeStatusAndCoinAccountId coinData =
+				new TradeWithoutTradeStatusAndCoinAccountId(coinPrice, coinCount);
+
+			CoinAccount coinAccount = new CoinAccount(
+				user.getId(),
+				FixtureGenerator.getDefaultLongArbitrary()
+					.between(0L, coinData.getCoinCount() * coinData.getCoinPrice() - 200000L)
+					.sample()
+			);
+
+			coinAccountRepository.save(coinAccount);
+
+			// when
+			BumawikiException exception = assertThrows(
+				BumawikiException.class,
+				() -> commandCoinService.buyCoin(coinData, user)
+			);
+
+			// then
+			assertThat(exception.getErrorCode()).isEqualTo(ErrorCode.MONEY_NOT_ENOUGH);
+		}
+
+		@RepeatedTest(REPEAT_COUNT)
+		void 구매_가격이_시세보다_높거나_같을_때() {
+			// given
+			Price price = priceRepository.getRecentPrice();
+
+			User user = FixtureGenerator.getDefaultUserBuilder().sample();
+
+			userRepository.save(user);
+
+			Long coinCount = 0L;
+			Long coinPrice = 0L;
+
+			while (coinCount * coinPrice <= 0) {
+				coinCount = FixtureGenerator.getDefaultLongArbitrary()
+					.greaterOrEqual(0L)
+					.sample();
+				coinPrice = FixtureGenerator.getDefaultLongArbitrary()
+					.greaterOrEqual(price.getPrice())
+					.sample();
+			}
+
+			TradeWithoutTradeStatusAndCoinAccountId coinData =
+				new TradeWithoutTradeStatusAndCoinAccountId(coinPrice, coinCount);
+
+			CoinAccount coinAccount = new CoinAccount(
+				user.getId(),
+				FixtureGenerator.getDefaultLongArbitrary()
+					.greaterOrEqual(coinData.getCoinCount() * coinData.getCoinPrice())
+					.sample()
+			);
+
+			coinAccountRepository.save(coinAccount);
+
+			Long moneyBeforeBuyCoin = coinAccount.getMoney();
+
+			// when
+			commandCoinService.buyCoin(coinData, user);
+
+			// then
+			assertAll(
+				() -> assertThat(tradeRepository.findAll().size()).isEqualTo(1),
+				() -> assertThat(
+					moneyBeforeBuyCoin >= coinData.getCoinPrice() * coinData.getCoinCount()
+				).isEqualTo(true),
+				() -> assertThat(
+					coinData.getCoinPrice() >= price.getPrice()
+				).isEqualTo(true)
+			);
+		}
+
+		@RepeatedTest(REPEAT_COUNT)
+		void 구매_가격이_시세보다_낮을_때() {
+			// given
+			Price price = priceRepository.getRecentPrice();
+
+			User user = FixtureGenerator.getDefaultUserBuilder().sample();
+
+			userRepository.save(user);
+
+			Long coinCount = 0L;
+			Long coinPrice = 0L;
+
+			while (coinCount * coinPrice <= 0) {
+				coinCount = FixtureGenerator.getDefaultLongArbitrary()
+					.greaterOrEqual(0L)
+					.sample();
+				coinPrice = FixtureGenerator.getDefaultLongArbitrary()
+					.between(0L, price.getPrice() - 1L)
+					.sample();
+			}
+
+			TradeWithoutTradeStatusAndCoinAccountId coinData =
+				new TradeWithoutTradeStatusAndCoinAccountId(coinPrice, coinCount);
+
+			CoinAccount coinAccount = new CoinAccount(
+				user.getId(),
+				FixtureGenerator.getDefaultLongArbitrary()
+					.greaterOrEqual(coinData.getCoinCount() * coinData.getCoinPrice())
+					.sample()
+			);
+
+			coinAccountRepository.save(coinAccount);
+
+			Long moneyBeforeBuyCoin = coinAccount.getMoney();
+
+			// when
+			commandCoinService.buyCoin(coinData, user);
+
+			// then
+			assertAll(
+				() -> assertThat(tradeRepository.findAll().size()).isEqualTo(1),
+				() -> assertThat(
+					moneyBeforeBuyCoin >= coinData.getCoinPrice() * coinData.getCoinCount()
+				).isEqualTo(true),
+				() -> assertThat(
+					coinData.getCoinPrice() < price.getPrice()
+				).isEqualTo(true)
+			);
+		}
 	}
 }
