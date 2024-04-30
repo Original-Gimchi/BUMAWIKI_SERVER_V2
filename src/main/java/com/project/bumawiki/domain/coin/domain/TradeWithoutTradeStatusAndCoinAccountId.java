f9acb32c1035cbff91a34749ab16f64b384540b8
마현우 <mhw35689402@gmail.com>
2024-04-30 21:58:05 +0900

    refactor(coin): NoArgsConstructor 삭제

--- src/main/java/com/project/bumawiki/domain/coin/domain/TradeWithoutTradeStatusAndCoinAccountId.java
+++ src/main/java/com/project/bumawiki/domain/coin/domain/TradeWithoutTradeStatusAndCoinAccountId.java
@@ -6,10 +6,8 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
 @AllArgsConstructor
 public class TradeWithoutTradeStatusAndCoinAccountId {
 	@NotNull
