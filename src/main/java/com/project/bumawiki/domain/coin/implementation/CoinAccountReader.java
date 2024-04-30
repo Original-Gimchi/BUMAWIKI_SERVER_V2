afa13f15bacc5578e90c85580df0b8ff725331a9
마현우 <mhw35689402@gmail.com>
2024-04-30 20:22:09 +0900

    refactor(coin): 중복 코드 제거

--- src/main/java/com/project/bumawiki/domain/coin/implementation/CoinAccountReader.java
+++ src/main/java/com/project/bumawiki/domain/coin/implementation/CoinAccountReader.java
@@ -7,8 +7,6 @@ import org.springframework.data.domain.Pageable;
 import com.project.bumawiki.domain.coin.domain.CoinAccount;
 import com.project.bumawiki.domain.coin.domain.repository.CoinAccountRepository;
 import com.project.bumawiki.global.annotation.Implementation;
-import com.project.bumawiki.global.error.exception.BumawikiException;
-import com.project.bumawiki.global.error.exception.ErrorCode;
 
 import lombok.RequiredArgsConstructor;
 
@@ -23,8 +21,7 @@ public class CoinAccountReader {
 	}
 
 	public CoinAccount getById(Long id) {
-		return coinAccountRepository.findById(id)
-			.orElseThrow(() -> new BumawikiException(ErrorCode.COIN_ACCOUNT_NOT_FOUND_EXCEPTION));
+		return coinAccountRepository.getById(id);
 	}
 
 	public CoinAccount getByUserId(Long userId) {
